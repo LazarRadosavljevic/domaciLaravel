8382ca613d28ff362f8628db52ff3c12573ae25c
Lazar Radosavljevic <radosavljeviclazar81@gmail.com>
2023-01-18 19:21:17 +0100

    Kreirani kontroleri i dodate rute

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\CoachController;
+use App\Http\Controllers\PlayerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/players',[PlayerController::class,'index']);
+
+Route::get('/coaches',[CoachController::class,'index']);
+
+Route::get('/teams',[TeamController::class,'index']);
